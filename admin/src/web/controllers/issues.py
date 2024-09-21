13437b0600f175b691e6eea7e092ba0437155acf
Franco Dalla Gasperina <francodalladg@gmail.com>
2024-09-20 22:43:53 -0300

    Revert "Se agregaron cambios para cumplir MVC"
    This reverts commit 01b702ed81e66c071a24e31f9d31d024fce4d7d9

--- admin/src/web/controllers/issues.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask import Blueprint
-from src.core import board 
-
-web_blueprint = Blueprint("issues",__name__, url_prefix="/src/web/templates")
-
-def issues_home():
-    return render_template("templates/home.html")
-    
