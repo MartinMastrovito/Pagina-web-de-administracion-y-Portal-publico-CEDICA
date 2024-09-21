c28a02a7247e2757b9bfcf0aa32e2e139a3e8b52
FrancoDalla <francodalladg@outlook.com>
2024-09-21 17:46:06 -0300

    Agregue una ruta como template folder porque no me encontraba las rutas cuando intentaba navegar

--- admin/src/core/__init__.py
+++ admin/src/core/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
 from src.web.handlers.error import not_found_error
+from src.web.handlers.error import internal_server_error
 from src.core import database
 from src.core.config import config
 
+
 def create_app(env = "development", static_folder = ""):
-	app = Flask(__name__)
+	app = Flask(__name__, template_folder='../web/templates')
         
 	app.config.from_object(config[env])
 	
@@ -16,7 +18,6 @@ def create_app(env = "development", static_folder = ""):
 	
 	@app.route("/") 
 	def home():
-
 		return render_template('home.html')
 
 	app.register_error_handler(404, not_found_error)
