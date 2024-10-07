13b82ce396a45393b9b21d538a0e1816b831c77b
FrancoDalla <francodalladg@outlook.com>
2024-10-07 00:48:34 -0300

    Avances en la creacion de cobros

--- admin/src/core/invoices/invoices.py
+++ admin/src/core/invoices/invoices.py
@@ -1,21 +1,23 @@
 from core.database import db
-from datetime import datetime
+import enum
+from sqlalchemy import Enum
 """
     modelo de cobros 
 """
-#lass Payment_methods(Payment_methods):
-#    cash = "efectivo"
-#    credit_card = "tarjeta de credito"
-#    debit_card = "tarjeta de débito"
-#    others = "otros"
+class payment_methods(enum.Enum):
+    name='payment_methods'
+    efectivo:1
+    tarjeta_credito:2
+    tarjeta_debito:3
+    otros:4
 
 class Invoices(db.Model):
     __tablename__="invoices"
     id = db.Column(db.Integer, primary_key=True)
     j_a = db.Column(db.Integer, db.ForeignKey("users.id"))
     pay_date = db.Column(db.Date)
-    payment_method = db.Column(db.Text)
+    payment_method = db.Column(Enum(payment_methods))
     amount = db.Column(db.Float)
     recipient = db.Column(db.Integer,db.ForeignKey("users.id"))
-    observations = db.Column(db.Text)
+    observations = db.Column(db.Text, nullable=True)
 
--- admin/src/web/controllers/invoices.py
+++ admin/src/web/controllers/invoices.py
@@ -1,5 +1,5 @@
-from flask import render_template, request, Blueprint
-from src.core.invoices.invoices import Invoices 
+from flask import render_template, request, Blueprint, redirect
+from src.core.invoices.invoices import Invoices
 from core.database import db
 
 """
@@ -17,6 +17,22 @@ def invoices_index():
     invoices = Invoices.query.all()
     return render_template("list_invoices.html", invoices=invoices)
 
-@invoices_bp.route("/crear-cobro")
+@invoices_bp.get("/crear-cobro")
 def invoice_create():
-    return render_template("create_invoice.html")
+    return render_template("create_invoice.html",invoices=invoices_bp)
+
+@invoices_bp.post("/crear-cobro")
+def create_user():
+    
+    invoice_information = {
+        "pay_date": request.form['pay_date'],
+        "amount": request.form['amount'],
+        "payment_method": request.form['payment_method'],
+        "j&a": request.form['j&a'],
+        "recipient": request.form['recipient'],
+        "observations": request.form['observations'],
+    }
+    
+    db.session.add(invoice_information)
+    db.session.commit()
+    return redirect('/usuarios')
