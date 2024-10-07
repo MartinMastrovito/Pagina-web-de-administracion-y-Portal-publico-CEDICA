4e763ef5f03abed6a7fd3a663a2bae2c475360d0
francodalladg <francodalladg@gmail.com>
2024-10-07 18:23:17 -0300

    Toque el estilo de como se muestra el listado. Cambie unas cosas para probar más facilmente de momento. También comence a ver como implementar el delete.

--- admin/src/core/invoices/invoices.py
+++ admin/src/core/invoices/invoices.py
@@ -5,19 +5,20 @@ from sqlalchemy import Enum
     modelo de cobros 
 """
 class payment_methods(enum.Enum):
-    name='payment_methods'
-    efectivo:1
-    tarjeta_credito:2
-    tarjeta_debito:3
-    otros:4
+    efectivo= 1
+    tarjeta_credito= 2
+    tarjeta_debito= 3
+    otros= 4
 
 class Invoices(db.Model):
     __tablename__="invoices"
     id = db.Column(db.Integer, primary_key=True)
-    j_a = db.Column(db.Integer, db.ForeignKey("users.id"))
+    #j_a = db.Column(db.Integer, db.ForeignKey("users.id")) de momento por falta de modelo no es asi
+    j_a = db.Column(db.Integer)
     pay_date = db.Column(db.Date)
-    payment_method = db.Column(Enum(payment_methods))
+    payment_method = db.Column(db.String)
     amount = db.Column(db.Float)
-    recipient = db.Column(db.Integer,db.ForeignKey("users.id"))
+    #recipient = db.Column(db.Integer,db.ForeignKey("users.id")) de momento por falta de modelo no es asi
+    recipient = db.Column(db.Integer)
     observations = db.Column(db.Text, nullable=True)
 
--- admin/src/web/controllers/invoices.py
+++ admin/src/web/controllers/invoices.py
@@ -12,27 +12,34 @@ invoices_bp = Blueprint("invoices", __name__,url_prefix="/cobros", template_fold
 def invoices_menu():
     return render_template("invoices_menu.html",invoices=invoices_bp)
 
-@invoices_bp.route("/listado")
+@invoices_bp.get("/lista-cobros")
 def invoices_index():
     invoices = Invoices.query.all()
-    return render_template("list_invoices.html", invoices=invoices)
+    return render_template("list_invoices.html", invoices=invoices,eliminado=True)
+
+@invoices_bp.post("/lista.cobros")
+def ola():
+    data_id = {
+        "id": request.form['id']
+    }
+    Invoices.delete().where(Invoices.id==id)
+    return redirect("/cobros/lista-cobros")
 
 @invoices_bp.get("/crear-cobro")
 def invoice_create():
     return render_template("create_invoice.html",invoices=invoices_bp)
 
 @invoices_bp.post("/crear-cobro")
-def create_user():
-    
+def create_invoice():
     invoice_information = {
         "pay_date": request.form['pay_date'],
         "amount": request.form['amount'],
         "payment_method": request.form['payment_method'],
-        "j&a": request.form['j&a'],
+        "j_a": request.form['j&a'],
         "recipient": request.form['recipient'],
         "observations": request.form['observations'],
     }
-    
+    invoice_information = Invoices(**invoice_information)
     db.session.add(invoice_information)
     db.session.commit()
-    return redirect('/usuarios')
+    return redirect('/cobros')
