eb612f31d0b581f94c5a7bb3c177299dbb0947f5
Evelyn Ramirez <eve.leonor99@gmail.com>
2024-10-17 16:25:30 -0300

    avances

--- admin/src/web/controllers/empleados.py
+++ admin/src/web/controllers/empleados.py
@@ -1,36 +1,44 @@
 from flask import Blueprint, request, jsonify
 from src.core.auth.models import Empleados
 from flask import render_template
+from src.core.database import db
 
 empleados_bp = Blueprint('empleados', __name__, url_prefix="/menu_empleados", template_folder='../templates/empleados',static_folder="/admin/static")
 @empleados_bp.get("/empleados")
+
 def show_empleado_form():
     return render_template("empleados/menu_empleados.html")
+
+#crear empleados
 
 @empleados_bp.get("/crear-empleado")
 def crear_empleado():
     return render_template("empleados/crear_empleado.html")
 
+
+# listar empleados
 @empleados_bp.route('/lista-empleados', methods=['GET'])
 def list_empleados():
-    empleados = Empleados.query.all() 
+    empleados = empleados.query.all() 
     return render_template('empleados/listar_empleados.html', empleados=empleados)
 
-# Update empleados
+# actualizar empleados
 @empleados_bp.route('/empleados/<int:id>', methods=['PUT'])
-def update_empleados(id):
-    data = request.get_json()
-    empleados = empleados.query.get(id)
-    if empleados:
+def actualizar_empleados(id):
+    empleado = Empleados.query.get(id)
+    
+    if empleado:
+        data = request.get_json()
         for key, value in data.items():
-            setattr(empleados, key, value)
+            setattr(empleado, key, value)
         db.session.commit()
-        return jsonify({'message': 'empleados updated successfully'})
-    return jsonify({'message': 'error emple'}), 404
+        return jsonify({'message': 'Empleado actualizado exitosamente'}), 200
+    return jsonify({'message': 'Empleado no encontrado'}), 404
 
-# Delete empleados
+
+# eliminar empleados
 @empleados_bp.route('/empleados/<int:id>', methods=['DELETE'])
-def delete_empleados(id):
+def eliminar_empleados(id):
     empleados = empleados.query.get(id)
     if empleados:
         db.session.delete(empleados)
