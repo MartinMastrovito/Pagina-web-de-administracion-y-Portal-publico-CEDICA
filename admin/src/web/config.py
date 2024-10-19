cdb14da1de7e08a4fb55098e575c94958766cbb7
Facundo <facundobarciabarate@gmail.com>
2024-10-19 04:59:47 -0300

    Configuracion de minio terminada

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -6,9 +6,17 @@ class Config(object):
     SECRET_KEY = "grupo30"
 
 class ProductionConfig(Config):
+    MINIO_SERVER = environ.get("MINIO_SERVER")
+    MINIO_ACCESS_KEY = environ.get("MINIO_ACCES_KEY")
+    MINIO_SECRET_KEY = environ.get("MINIO_SECRET_KEY")
+    MINIO_SECURE = True
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
 
 class DevelopmentConfig(Config):
+    MINIO_SERVER = "localhost:9000"
+    MINIO_ACCESS_KEY = "9H3ZLLlZC4qX1vbZ1MJb"
+    MINIO_SECRET_KEY = "mJGy8It49ebNNu2C1PaFMe5g1s6ObJ9adpzAHsWC"
+    MINIO_SECURE = False
     DB_USER = os.getenv("DB_USER", os.getlogin())
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     DB_HOST = "localhost"
