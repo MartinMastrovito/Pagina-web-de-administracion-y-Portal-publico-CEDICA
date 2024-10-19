75b0902d44850b78206e8dd90584360cb8439d38
FrancoDalla <francodalladg@gmail.com>
2024-10-19 10:44:08 -0300

    Cambios al modelo de Cobros

--- admin/src/core/invoices/invoices.py
+++ admin/src/core/invoices/invoices.py
@@ -6,12 +6,11 @@ from sqlalchemy import Enum, ForeignKey
 class Invoices(db.Model):
     __tablename__="invoices"
     id = db.Column(db.Integer, primary_key=True)
-    #j_a = db.Column(db.Integer, db.ForeignKey("ja.id")) de momento por falta de modelo no es asi
-    j_a = db.Column(db.Integer)
+    j_a = db.Column(db.Integer, db.ForeignKey("JYA.id"))
     pay_date = db.Column(db.Date)
     payment_method = db.Column(db.String)
     amount = db.Column(db.Float)
-    #recipient = db.Column(db.Integer,db.ForeignKey("users.id")) de momento por falta de modelo no es asi
+    #recipient = db.Column(db.Integer,db.ForeignKey("users.id")) Falta de modelo
     recipient = db.Column(db.Integer)
     observations = db.Column(db.Text, nullable=True)
 
