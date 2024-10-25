532f1c3b74b4adc2e20e7322fa6673d9ab1ab2a0
Evelyn Ramirez <eve.leonor99@gmail.com>
2024-10-25 02:23:43 -0300

    avances en la funcionalidad pagos

--- admin/src/core/auth/models/model_pago.py
+++ admin/src/core/auth/models/model_pago.py
@@ -1,16 +1,19 @@
 from src.core.database import db
 
-
+"""
 
 beneficiary = db.Table('beneficiary',
     db.Column('pago_id', db.Integer, db.ForeignKey('pago.id'), primary_key=True),
     db.Column('empleados_id', db.Integer, db.ForeignKey('empleados.id'), primary_key=True),
     extend_existing=True
 )
-
+"""
 class Pago(db.Model):
     __tablename__ = 'pago'  
     __table_args__ = {'extend_existing': True}
+
+    
+
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     beneficiario_id = db.Column(db.Integer, db.ForeignKey('empleados.id'), nullable=True)  # Empleado dado de alta
@@ -19,8 +22,5 @@ class Pago(db.Model):
     tipo_pago = db.Column(db.String, nullable=False)  # Honorarios, proveedor o gastos varios
     description = db.Column(db.String, nullable=True)
     
-    
-    empleados = db.relationship("src.core.auth.models.model_empleado.Empleados", secondary='beneficiary', back_populates='beneficiary')
-
-
+    empleados = db.relationship("Empleados", back_populates='pagos')
 
--- admin/src/web/controllers/empleados.py
+++ admin/src/web/controllers/empleados.py
@@ -2,9 +2,9 @@ from flask import Blueprint, request, jsonify
 from src.core.auth.models.model_empleado import Empleados
 from flask import render_template , flash, redirect, current_app
 from src.core.database import db
-from sqlalchemy import asc, desc
 from core.auth.decorators import login_required 
 from werkzeug.utils import secure_filename
+from sqlalchemy import asc, desc
 import os
 
 empleados_bp = Blueprint('empleados', __name__, url_prefix="/menu_empleados", template_folder='../templates/empleados',static_folder="/admin/static")
@@ -119,7 +119,7 @@ def actualizar_empleados(id):
 # eliminar empleados
 @empleados_bp.route('/empleados/<int:id>', methods=['DELETE'])
 def eliminar_empleados(id):
-    empleados = empleados.query.get(id)
+    empleados = Empleados.query.get(id)
     if empleados:
         db.session.delete(empleados)
         db.session.commit()
