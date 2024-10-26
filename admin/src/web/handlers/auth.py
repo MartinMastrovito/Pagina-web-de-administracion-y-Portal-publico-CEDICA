70f817b4e8f70d1ba417686dceb25837d1c3af4f
FrancoDalla <francodalladg@gmail.com>
2024-10-25 23:36:38 -0300

    Agregados para manejo de permisos

--- admin/src/web/handlers/auth.py
+++ admin/src/web/handlers/auth.py
@@ -3,18 +3,15 @@ from src.core.auth import utiles
 
 
 def check_permission(session, permission):
-    # Temporariamente anulado el chequeo de permisos
-    return True
 
     # Código original
-    user_email = session.get("user")
-    user = utiles.get_user_by_email(user_email)
+    user = utiles.get_user(session)
 
     if user is None:
         return False
 
     # Obtenemos los permisos desde la base de datos
-    permissions = utiles.get_permissions(user)
+    permissions = utiles.get_permissions(user.id)
     print(f"Permisos del usuario: {permissions}")
 
     # Verificamos si el permiso solicitado está en la lista de permisos del usuario
