5b937efabdf1ebce77f13b9a3c37d748f93f52ca
FrancoDalla <francodalladg@gmail.com>
2024-10-29 11:34:55 -0300

    Cambios al seed. Agrego cambios para que se agreguen de otra forma

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -39,24 +39,17 @@ def role_create():
 
 def permission_create():
     # Creación de las instancias de permisos con sus respectivos nombres
-    user_index = Permission(name="user_index")
-    user_new = Permission(name="user_new")
-    user_destroy = Permission(name="user_destroy")
-    user_show = Permission(name="user_show")
-    jya_index = Permission(name="jya_index")
-    jya_new = Permission(name="jya_new")
-    jya_destroy = Permission(name="jya_destroy")
-    jya_update = Permission(name="jya_update")
-    jya_show = Permission(name="jya_show")
-    db.session.add(user_index)
-    db.session.add(user_new)
-    db.session.add(user_destroy)
-    db.session.add(user_show)
-    db.session.add(jya_index)
-    db.session.add(jya_new)
-    db.session.add(jya_destroy)
-    db.session.add(jya_update)
-    db.session.add(jya_show)
+    agregados = [
+    Permission(name="user_index"),
+    Permission(name="user_new"),
+    Permission(name="user_destroy"),
+    Permission(name="user_show"),
+    Permission(name="jya_index"),
+    Permission(name="jya_new"),
+    Permission(name="jya_destroy"),
+    Permission(name="jya_update"),
+    Permission(name="jya_show"), ]
+    db.session.add_all(agregados)
     db.session.commit()
 
 def db_seeds():
