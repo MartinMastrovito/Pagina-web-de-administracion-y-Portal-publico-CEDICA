4ce7a2b9666d2d661b38450b14dda9d47a5e33d9
FrancoDalla <francodalladg@gmail.com>
2024-10-29 11:29:36 -0300

    Avance en el seed para la BD. Se agrego la creacion de permisos

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -1,4 +1,5 @@
 from src.core.auth.models.model_user import Role
+from src.core.auth.models.model_permission import Permission
 from src.core.database import db
 
 def role_create():
@@ -36,10 +37,28 @@ def role_create():
     db.session.commit()
 
 
-
-
-
-
+def permission_create():
+    # Creación de las instancias de permisos con sus respectivos nombres
+    user_index = Permission(name="user_index")
+    user_new = Permission(name="user_new")
+    user_destroy = Permission(name="user_destroy")
+    user_show = Permission(name="user_show")
+    jya_index = Permission(name="jya_index")
+    jya_new = Permission(name="jya_new")
+    jya_destroy = Permission(name="jya_destroy")
+    jya_update = Permission(name="jya_update")
+    jya_show = Permission(name="jya_show")
+    db.session.add(user_index)
+    db.session.add(user_new)
+    db.session.add(user_destroy)
+    db.session.add(user_show)
+    db.session.add(jya_index)
+    db.session.add(jya_new)
+    db.session.add(jya_destroy)
+    db.session.add(jya_update)
+    db.session.add(jya_show)
+    db.session.commit()
 
 def db_seeds():
     role_create()
+    permission_create()
