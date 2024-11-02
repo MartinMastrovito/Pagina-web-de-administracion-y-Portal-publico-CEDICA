3ba90f1138c4be2e19e60a83107377a165b8c789
FrancoDalla <francodalladg@gmail.com>
2024-11-02 19:13:32 -0300

    Avances en el seeds Ahora crea todos los permisos y los asocia a los respectivos roles.

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -1,57 +1,219 @@
-from src.core.auth.models.model_user import Role
-from src.core.auth.models.model_permission import Permission
+from src.core.auth.models.model_user import Role, User
+from src.core.auth.models.model_permission import Permission, RolePermission
 from src.core.database import db
+from src.core import bcrypt
+from src.core.auth import utiles
+
 
 def role_create():
     """
-        Carga los roles en la base de datos
+    Carga los roles en la base de datos
     """
-    
-    #Roles del sistema
-    tecnica = Role(
-        id = 1,
-        name = "tecnica"
-    )
-    ecuestre = Role(
-        id = 2,
-        name = "ecuestre"
-    )
-    voluntariado = Role(
-        id = 3,
-        name = "voluntariado"
-    )
-    administracion = Role(
-        id =4,
-        name = "administracion" 
-    )
-    sysadmin = Role(
-        id = 5,
-        name ="sysadmin" 
-    )
 
-    db.session.add(tecnica)
-    db.session.add(ecuestre)
-    db.session.add(voluntariado)
-    db.session.add(administracion)
-    db.session.add(sysadmin)
+    # Roles del sistema
+    list_role = [
+        Role(name="tecnica"),
+        Role(name="ecuestre"),
+        Role(name="voluntariado"),
+        Role(name="administracion"),
+        Role(name="sysadmin"),
+    ]
+
+    db.session.add_all(list_role)
     db.session.commit()
 
 
 def permission_create():
     # Creación de las instancias de permisos con sus respectivos nombres
-    agregados = [
-    Permission(name="user_index"),
-    Permission(name="user_new"),
-    Permission(name="user_destroy"),
-    Permission(name="user_show"),
-    Permission(name="jya_index"),
-    Permission(name="jya_new"),
-    Permission(name="jya_destroy"),
-    Permission(name="jya_update"),
-    Permission(name="jya_show"), ]
-    db.session.add_all(agregados)
+    list_permission = [
+        # permisos usuarios
+        Permission(name="user_index"),
+        Permission(name="user_new"),
+        Permission(name="user_destroy"),
+        Permission(name="user_update"),
+        Permission(name="user_show"),
+        # permisos jya
+        Permission(name="jya_index"),
+        Permission(name="jya_new"),
+        Permission(name="jya_destroy"),
+        Permission(name="jya_update"),
+        Permission(name="jya_show"),
+        # permisos cobros
+        Permission(name="invoice_index"),
+        Permission(name="invoice_new"),
+        Permission(name="invoice_destroy"),
+        Permission(name="invoice_update"),
+        Permission(name="invoice_show"),
+        Permission(name="invoice_menu"),
+        # permisos pagos
+        Permission(name="payment_index"),
+        Permission(name="payment_new"),
+        Permission(name="payment_destroy"),
+        Permission(name="payment_update"),
+        Permission(name="payment_show"),
+        # permisos equipo
+        Permission(name="team_index"),
+        Permission(name="team_new"),
+        Permission(name="team_destroy"),
+        Permission(name="team_update"),
+        Permission(name="team_show"),
+        # permisos caballos 
+        Permission(name="horse_index"),
+        Permission(name="horse_new"),
+        Permission(name="horse_destroy"),
+        Permission(name="horse_update"),
+        Permission(name="horse_show"),
+
+    ]
+    db.session.add_all(list_permission)
     db.session.commit()
+
+def tecnica_rol_create():
+    tecnica_id = Role.query.filter(Role.name =="tecnica").first().id
+    permissions = Permission.query.filter(
+       db.or_(
+        Permission.name.contains("jya"),
+        Permission.name == "horse_index",
+        Permission.name == "horse_show",
+        Permission.name == "Invoice_menu",
+        Permission.name == "Invoice_index",
+        Permission.name == "Invoice_show"
+       )
+    )
+    tecnica_permissions = []
+    for permission in permissions:
+        tecnica_permissions.append(RolePermission(role_id = tecnica_id ,permission_id= permission.id))
+    db.session.add_all(tecnica_permissions)
+    db.session.commit()
+
+def ecuestre_rol_create():
+    ecuestre_id = Role.query.filter(Role.name =="ecuestre").first().id
+    permissions = Permission.query.filter(
+       db.or_(
+        Permission.name.contains("horse"),
+        Permission.name == "jya_index",
+        Permission.name == "jya_show"
+       )
+    )
+    ecuestre_permissions = []
+    for permission in permissions:
+        ecuestre_permissions.append(RolePermission(role_id = ecuestre_id ,permission_id= permission.id))
+    db.session.add_all(ecuestre_permissions)
+    db.session.commit()
+
+
+def administracion_rol_create():
+    administrator_id = Role.query.filter(Role.name == "administracion").first().id
+    permissions = Permission.query.filter(
+       db.or_(
+        Permission.name.contains("team"),
+        Permission.name.contains("invoice"),
+        Permission.name.contains("payment"),
+        Permission.name.contains("jya"),
+        Permission.name == "horse_index",
+        Permission.name == "horse_show"
+       )
+    )
+    administration_permissions = []
+    for permission in permissions:
+        administration_permissions.append(RolePermission(role_id = administrator_id ,permission_id= permission.id))
+    db.session.add_all(administration_permissions)
+    db.session.commit()
+
+def sysadmin_rol_create():
+    permissions = Permission.query.all()
+    sysadmin_permissions = []
+    for permission in permissions:
+        sysadmin_permissions.append(RolePermission(role_id = 5, permission_id = permission.id))
+    db.session.add_all(sysadmin_permissions)
+    db.session.commit()
+    
+
+
+def rolePermission_create():
+    sysadmin_rol_create()
+    administracion_rol_create()
+    ecuestre_rol_create()
+    tecnica_rol_create()
+
+
+def user_create():
+    list_user = [
+        User(
+            email="sysadmin@test.com",
+            password=bcrypt.generate_password_hash("sysadmin"),
+            alias="sysadmin",
+            enabled=True,
+            role_id=5,
+            created_at="2024-10-29",
+        ),
+        User(
+            email="administracion@test.com",
+            password=bcrypt.generate_password_hash("administracion"),
+            alias="administracion",
+            enabled=True,
+            role_id=4,
+            created_at="2024-10-29",
+        ),
+    ]
+    db.session.add_all(list_user)
+    db.session.commit()
+
 
 def db_seeds():
     role_create()
     permission_create()
+    user_create()
+    rolePermission_create()
+
+
+
+"""
+Técnica
+    jya_index
+    jya_show
+    jya_update
+    jya_create
+    jya_destroy
+    caballos_index
+    caballos_show
+    invoices_menu
+    invoices_index
+    invoices_show
+Ecuestre
+    jya_index
+    jya_show
+    caballos_index
+    caballos_show
+    caballos_update
+    caballos_create
+    caballos_destroy
+        
+Voluntariado:
+Administración:
+    team_index
+    team_show
+    team_update
+    team_create
+    team_destroy
+    payment_index
+    payment_show
+    payment_update
+    payment_create
+    payment_destroy
+    jya_index
+    jya_show
+    jya_update
+    jya_create
+    jya_destroy
+    caballos_index
+    caballos_show
+    invoice_index
+    invoice_show
+    invoice_update
+    invoice_create
+    invoice_destroy
+    invoice_menu
+Sysadmin:todo
+
+"""
