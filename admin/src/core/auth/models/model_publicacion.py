dab1f2e426a5db1b177ab350aed988d723baf6f8
Facundo <facundobarciabarate@gmail.com>
2024-11-14 06:21:23 -0300

    Casos donde no hay datos solucionados

---
+++ admin/src/core/auth/models/model_publicacion.py
@@ -0,0 +1,20 @@
+from src.core.database import db
+from datetime import datetime, timezone
+
+class Publicacion(db.Model):
+    __tablename__ = "publicaciones"
+
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_publicacion = db.Column(db.Date, nullable=True)
+    fecha_creacion = db.Column(db.Date, default=datetime.now(timezone.utc), nullable=False)
+    fecha_actualizacion = db.Column(db.Date, onupdate=datetime.now(timezone.utc), nullable=True)
+    titulo = db.Column(db.String(25), nullable=False)
+    copete = db.Column(db.String(255), nullable=True)
+    contenido = db.Column(db.Text, nullable=False)
+    autor_id = db.Column(db.Integer, db.ForeignKey("empleados.id"), nullable=False)
+    estado = db.Column(db.Enum("Borrador", "Publicado", "Archivado", name="estado_publicacion"), default="Borrador")
+
+    autor = db.relationship("Empleados", backref=db.backref("publicaciones", lazy=True))
+
+    def __repr__(self):
+        return f"<Publicacion {self.titulo}>"
