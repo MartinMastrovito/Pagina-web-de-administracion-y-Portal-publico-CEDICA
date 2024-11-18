a471aad4715197b7058eceb8a2c2482436a431bc
FrancoDalla <francodalladg@gmail.com>
2024-11-18 18:54:19 -0300

    Finalización del mostrado de noticias. Ya cumple, al menos de forma basica, mostrar noticias individualmente. Todavía queda cumplir filtrar por ciertos parametros.

--- portal/src/views/articleShow.vue
+++ portal/src/views/articleShow.vue
@@ -1,14 +1,16 @@
 <script setup>
     import { useArticlesStore } from '../stores/articles';
     import { storeToRefs } from 'pinia';
-    import { onMounted, onBeforeMount} from 'vue';
+    import { onMounted } from 'vue';
+    import { useRoute } from 'vue-router';
 
     const store = useArticlesStore();
     const { articles, loading, error} = storeToRefs(store);
+    const route = useRoute();
 
     const fetchArticles = async() => {
         const filter = {
-            id:params.id,
+            id:route.params.id,
         }
         await store.fetchArticles(filter);
     };
@@ -20,9 +22,9 @@
 </script>
 
 <template>
-    <h1>ID: {{this.$route.params.id}}</h1>
-    <h1>titulo</h1>
-    <h2>copete</h2>
-    <span>año</span>
-    <p>contenido</p>
+    <h1>ID: {{route.params.id}}</h1>
+    <h1>{{articles.titulo}}</h1>
+    <h2>{{articles.copete}}</h2>
+    <span>{{articles.año}}</span>
+    <p>{{articles.contenido}}</p>
 </template>
