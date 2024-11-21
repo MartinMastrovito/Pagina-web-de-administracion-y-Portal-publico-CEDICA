eff9811aa53b4d6787784beb7cccb843512601ce
FrancoDalla <francodalladg@gmail.com>
2024-11-21 03:16:28 -0300

    Cambios en el listado de articulos Ahora el componente lista  se muestra mediante props que toma la vista con la api

--- portal/src/components/articlesList.vue
+++ portal/src/components/articlesList.vue
@@ -1,36 +1,15 @@
 <script setup>
     import articleItem from '../components/articleItem.vue'
-    import { useArticlesStore } from '../stores/articles';
-    import { storeToRefs } from 'pinia';
-    import { onMounted, } from 'vue';
-    import { RouterLink, useRoute } from 'vue-router';
-
-    const store = useArticlesStore();
-    const { articles, pages,loading, error} = storeToRefs(store);
-    const route = useRoute();
-    
-    const fetchArticles = async() => {
-        const filter = {
-            page:route.params.page,
-        }
-        await store.fetchArticles(filter);
-    };
-    onMounted(() => {
-        if(!articles.length){
-            fetchArticles();
-        }
-    });
-
-
-
+    defineProps({
+        articles:Array,
+        pages:Number
+    })
 </script>
 
 <template>
     <div>
         <h2>Lista de articulos</h2>
-        <p v-if="loading">Cargando...</p>
-        <p v-if="error">{{ error }}</p>
-        <div v-if="!loading && articles.length">
+        <div v-if="articles.length">
             <div class="listado">
                 <articleItem 
                     v-for="article in articles" 
@@ -41,16 +20,7 @@
                     :contenido="article.contenido"
                 />
             </div>
-            <nav>
-                <RouterLink
-                    v-for="page in pages"
-                    class="btn" 
-                    :to="`/noticias/${page}`">   
-                        {{ page }}
-                </RouterLink>
-            </nav>
         </div>
-        <p v-if="!loading && !articles.length">No hay articulos para mostrar</p>
     </div>
 </template>
 
