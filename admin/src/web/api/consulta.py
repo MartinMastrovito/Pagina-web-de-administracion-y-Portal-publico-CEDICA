0b1273e07a38e1e7a321a218ea5ca87257f6ffcc
MartinMastrovito <martinmastrovito@gmail.com>
2024-11-21 14:26:46 -0300

    captcha

--- admin/src/web/api/consulta.py
+++ admin/src/web/api/consulta.py
@@ -1,45 +1,49 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+import requests
+from flask import Blueprint, jsonify, request
 from src.core import crud_consultas as crud_consulta
-from src.core.auth.decorators import login_required, check
-from datetime import datetime
 
 consulta_api_bp = Blueprint("consulta_api", __name__, url_prefix="/api/consulta")
+
+# Tu clave secreta de reCAPTCHA
+RECAPTCHA_SECRET_KEY = "6LfMIYYqAAAAAG9SdB6g7AfOJaALFZVDfl37N2ci"
 
 @consulta_api_bp.route('', methods=['POST'])
 def crear_consulta_portal():
     data = request.get_json()
-
     if not data:
         return jsonify({"error": "No se enviaron datos"}), 400
-
-    # Imprimir los datos para confirmar qué estamos recibiendo
-    print(f"Datos recibidos: {data}")
 
     nombre_completo = data.get('nombre_completo')
     email = data.get('email')
     mensaje = data.get('mensaje')
-    captcha = data.get('captcha')
+    captcha = data.get('captcha')  # Token de reCAPTCHA
     estado = data.get('estado')
     fecha = data.get('fecha')
 
-    # Verifica si hay algún campo faltante
-    if not nombre_completo or not email or not mensaje or not captcha or not estado or not fecha:
+    if not all([nombre_completo, email, mensaje, captcha, estado, fecha]):
         return jsonify({"error": "Faltan campos obligatorios"}), 400
 
-    # Verifica si el captcha es correcto
-    if captcha != "8":
-        return jsonify({"error": "Captcha incorrecto"}), 400
+    # Verificar el token de reCAPTCHA
+    recaptcha_response = requests.post(
+        "https://www.google.com/recaptcha/api/siteverify",
+        data={
+            "secret": RECAPTCHA_SECRET_KEY,
+            "response": captcha,
+        },
+    )
 
-    # Procede con la creación de la consulta
+    recaptcha_result = recaptcha_response.json()
+    if not recaptcha_result.get("success"):
+        return jsonify({"error": "Captcha inválido"}), 400
+
+    # Proceder con la creación de la consulta
     datos_consulta = {
-        'nombre_completo': nombre_completo,
-        'email': email,
-        'fecha': fecha,
-        'descripcion': mensaje,
-        'estado': estado
+        "nombre_completo": nombre_completo,
+        "email": email,
+        "fecha": fecha,
+        "descripcion": mensaje,
+        "estado": estado,
     }
-
-    # Verifica si la creación fue exitosa
     nueva_consulta = crud_consulta.create_consulta(**datos_consulta)
 
     return jsonify({"message": "Consulta guardada correctamente"}), 201
