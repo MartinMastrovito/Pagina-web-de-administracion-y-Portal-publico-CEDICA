d0aa7f052b10f0562a8b3203fb7d1f41f019cc16
MartinMastrovito <martinmastrovito@gmail.com>
2024-11-22 12:24:08 -0300

    modificacion de la separacion

--- admin/src/core/crud_pagos/__init__.py
+++ admin/src/core/crud_pagos/__init__.py
@@ -1,26 +1,30 @@
 from src.core.database import db
 from src.core.auth.models.model_pago import Pago
+from datetime import datetime
 
-def listar_pagos():
-    return Pago.query.all()
+def obtener_pagos(filtros):
+    query = Pago.query
+    if filtros.get('fecha_inicio') and filtros.get('fecha_fin'):
+        query = query.filter(Pago.fecha_pago.between(filtros['fecha_inicio'], filtros['fecha_fin']))
+    if filtros.get('tipo_pago'):
+        query = query.filter(Pago.tipo_pago == filtros['tipo_pago'])
+    orden = filtros.get('orden', 'asc')
+    query = query.order_by(Pago.fecha_pago.asc() if orden == 'asc' else Pago.fecha_pago.desc())
+    return query.all()
 
-def obtener_pago(id):
-    return Pago.query.get_or_404(id)
-
-def crear_pago(data):
-    nuevo_pago = Pago(**data)
-    db.session.add(nuevo_pago)
+def crear_pago(datos):
+    pago = Pago(**datos)
+    db.session.add(pago)
     db.session.commit()
-    return nuevo_pago
+    return pago
 
-def actualizar_pago(id, data):
-    pago = Pago.query.get_or_404(id)
-    for key, value in data.items():
+def actualizar_pago(pago_id, datos):
+    pago = Pago.query.get_or_404(pago_id)
+    for key, value in datos.items():
         setattr(pago, key, value)
     db.session.commit()
     return pago
 
-def eliminar_pago(id):
-    pago = Pago.query.get_or_404(id)
-    db.session.delete(pago)
+def eliminar_pago(pago_id):
+    Pago.query.filter_by(id=pago_id).delete()
     db.session.commit()
--- admin/src/web/controllers/pagos.py
+++ admin/src/web/controllers/pagos.py
@@ -1,31 +1,50 @@
-from flask import Blueprint, request, jsonify
-from src.core.crud_pagos import listar_pagos, obtener_pago, crear_pago, actualizar_pago, eliminar_pago
+from flask import Blueprint, render_template, request, redirect, flash
+from src.core.crud_pagos import obtener_pagos, crear_pago, actualizar_pago, eliminar_pago
+from src.core.invoices.utiles import get_all_employees
 
-pagos_bp = Blueprint('pagos', __name__, url_prefix='/pagos')
+pago_bp = Blueprint("pago", __name__, url_prefix="/pago", template_folder='../templates/pagos', static_folder="/admin/static")
 
-@pagos_bp.route('/', methods=['GET'])
-def listar_pagos_route():
-    pagos = listar_pagos()
-    return jsonify([pago.to_dict() for pago in pagos])
+@pago_bp.route('/listar', methods=['GET'])
+def listar_pago():
+    filtros = {
+        'fecha_inicio': request.args.get('fecha_inicio'),
+        'fecha_fin': request.args.get('fecha_fin'),
+        'tipo_pago': request.args.get('tipo_pago'),
+        'orden': request.args.get('orden', 'asc')
+    }
+    pagos = obtener_pagos(filtros)
+    return render_template('pagos/listar_pago.html', pagos=pagos)
 
-@pagos_bp.route('/<int:id>', methods=['GET'])
-def obtener_pago_route(id):
-    pago = obtener_pago(id)
-    return jsonify(pago.to_dict())
-
-@pagos_bp.route('/', methods=['POST'])
+@pago_bp.route('/crear', methods=['GET', 'POST'])
 def crear_pago_route():
-    data = request.get_json()
-    nuevo_pago = crear_pago(data)
-    return jsonify(nuevo_pago.to_dict()), 201
+    if request.method == 'POST':
+        datos = {
+            "beneficiario_id": request.form.get('beneficiario_id'),
+            "monto": request.form['monto'],
+            "fecha_pago": request.form['fecha_pago'],
+            "tipo_pago": request.form['tipo_pago'],
+            "descripcion": request.form.get('descripcion', "")
+        }
+        crear_pago(datos)
+        flash("Pago creado exitosamente", 'success')
+        return redirect('/pago/listar')
+    empleados = get_all_employees()
+    return render_template('pagos/crear_pago.html', empleados=empleados)
 
-@pagos_bp.route('/<int:id>', methods=['PUT'])
-def actualizar_pago_route(id):
-    data = request.get_json()
-    pago = actualizar_pago(id, data)
-    return jsonify(pago.to_dict())
+@pago_bp.route('/actualizar/<int:pago_id>', methods=['POST'])
+def actualizar_pago_route(pago_id):
+    datos = {
+        "monto": request.form['monto'],
+        "tipo_pago": request.form['tipo_pago'],
+        "descripcion": request.form.get('descripcion', "")
+    }
+    actualizar_pago(pago_id, datos)
+    flash("Pago actualizado correctamente", 'success')
+    return redirect('/pago/listar')
 
-@pagos_bp.route('/<int:id>', methods=['DELETE'])
-def eliminar_pago_route(id):
-    eliminar_pago(id)
-    return '', 204
+@pago_bp.route('/eliminar', methods=['POST'])
+def eliminar_pago_route():
+    pago_id = request.form['id']
+    eliminar_pago(pago_id)
+    flash("Pago eliminado correctamente", 'success')
+    return redirect('/pago/listar')
