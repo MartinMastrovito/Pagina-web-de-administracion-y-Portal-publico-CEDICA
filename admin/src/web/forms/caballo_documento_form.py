3d1abbaa09e325ec478ca4628fb84d6ad6f178de
Facundo <facundobarciabarate@gmail.com>
2024-11-21 21:29:11 -0300

    mas docstrings para documentar

--- admin/src/web/forms/caballo_documento_form.py
+++ admin/src/web/forms/caballo_documento_form.py
@@ -4,13 +4,11 @@ from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileAllowed
 
 class CaballoDocumentoForm(FlaskForm):
-    # Título del documento
     titulo = StringField('Título del Documento', validators=[
         DataRequired(),
         Length(min=2, max=255)
     ])
-    
-    # Tipo de documento: Ficha, Informe, Imágenes, etc.
+
     tipo_documento = SelectField('Tipo de Documento', choices=[
         ('ficha', 'Ficha general del caballo'),
         ('planificacion', 'Planificación de Entrenamiento'),
@@ -19,11 +17,9 @@ class CaballoDocumentoForm(FlaskForm):
         ('veterinario', 'Registro Veterinario')
     ], validators=[DataRequired()])
     
-    # Archivo que se va a subir
     archivo = FileField('Archivo', validators=[
         DataRequired(),
         FileAllowed(['pdf', 'jpg', 'jpeg', 'png'], 'Solo se permiten archivos PDF o imágenes (JPG, PNG)')
     ])
 
-    # Botón para enviar el formulario
     submit = SubmitField('Subir Documento')
